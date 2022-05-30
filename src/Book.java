699b1ce49e460f9baeaa0a09a5e29177ea2a7b85
Kirill Chetverikov <MaraZmOo@yandex.ru>
2022-05-30 15:33:39 +0300

    Создал класс Book и метод get/set

---
+++ src/Book.java
@@ -0,0 +1,16 @@
+public class Book { String title;
+    int numberOfPages;
+    int yearOfRelease;
+    String author;
+
+    void setTitle(String title){
+        this.title = title;
+    }
+
+    String getTitle(){
+        return title;
+    }
+
+
+}
+
